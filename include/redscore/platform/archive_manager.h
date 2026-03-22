0fda9e3d43ec251ccb21f43cce3beac12a14ca74
REDxEYE <med45c@gmail.com>
2026-03-22 20:53:34 +0300

    Move load_archive_callback to protected in ArchiveManager

--- include/redscore/platform/archive_manager.h
+++ include/redscore/platform/archive_manager.h
@@ -9,11 +9,11 @@
 #include <unordered_set>
 #include "redscore/platform/archive.h"
 
-class ArchiveManager: public Archive{
+class ArchiveManager : public Archive {
+protected:
     using load_archive_callback = std::function<std::pair<bool, uint64>(ArchiveManager &manager, uint64 hash)>;
 
 public:
-
     explicit ArchiveManager(load_archive_callback load_archive)
         : m_load_archive(std::move(load_archive)) {
     }
@@ -23,13 +23,16 @@ public:
     void mount(std::unique_ptr<Archive> archive);
 
     void unmount(uint64 archive_hash);
+
     void unmount(std::string_view name);
 
     [[nodiscard]] bool has_file(uint64 hash) override;
+
     [[nodiscard]] bool has_file(std::string_view name) override;
 
 
     std::unique_ptr<IO::File> get_file(uint64 hash) override;
+
     std::unique_ptr<IO::File> get_file(std::string_view name) override;
 
     void all_entries(std::vector<ArchiveEntry> &entries) const override;
@@ -43,23 +46,27 @@ public:
     }
 
     ArchiveManager() = delete;
-    ArchiveManager(const ArchiveManager&) = delete;
-    ArchiveManager& operator=(const ArchiveManager&) = delete;
 
-    ArchiveManager(ArchiveManager&&) noexcept = default;
-    ArchiveManager& operator=(ArchiveManager&&) noexcept = default;
+    ArchiveManager(const ArchiveManager &) = delete;
+
+    ArchiveManager &operator=(const ArchiveManager &) = delete;
+
+    ArchiveManager(ArchiveManager &&) noexcept = default;
+
+    ArchiveManager &operator=(ArchiveManager &&) noexcept = default;
 
 protected:
-    std::unordered_map<uint64, std::unique_ptr<Archive>> m_archives;
+    std::unordered_map<uint64, std::unique_ptr<Archive> > m_archives;
     load_archive_callback m_load_archive;
 
     static constexpr size_t MAX_DYNAMIC_MOUNTS = 16;
 
     void touch_dynamic_mount(uint64 hash);
+
     void evict_dynamic_mounts();
+
     void forget_dynamic_mount(uint64 hash);
+
     std::deque<uint64> m_dynamic_mount_order;
     std::unordered_set<uint64> m_dynamic_mount_set;
-
 };
-
